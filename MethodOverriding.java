ad7f6725ee7870b019dacfd0038682741f12c92a
Deepa <srajesh.deepa@gmail.com>
2021-11-18 21:44:09 +0530

    MethodOverriding. java resolved conflicts.  Please enter the commit message for your changes. Lines starting

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,10 +1,12 @@
+
 public class MethodOverriding {
+// method to add two integers
 
   static int addition(int num1, int num2){
        int res = num1 + num2 ;
        return(res);
     }
-
+// method to concatenate strings
    static String addition(String str1, String str2){
        String res = str1 + str2;
        return(res);
