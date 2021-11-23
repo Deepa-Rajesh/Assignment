7577927b995c3601b38ade1ecbd7ae477a1f9179
Deepa <srajesh.deepa@gmail.com>
2021-11-23 21:07:29 +0530

    Updated version

--- BankDenomination.java
+++ BankDenomination.java
@@ -1,17 +1,17 @@
 // Java assignment for Abstract class
 // class with abstract method
-abstract class AbstractMain {
+abstract class BankDenomination {
    // Abstract method with signature
     public abstract void denominations();
 }
 // base class using abstract method and providing body to method
-class SbiBank extends AbstractMain{
+class SbiBank extends BankDenomination {
     public void denominations(){
         System.out.println("Available denomination in SBI: 2000, 500, 200" );
     }
 }
 // base class using abstract method and providing body to method
-class IciciBank extends AbstractMain{
+class IciciBank extends BankDenomination {
     public void denominations(){
         System.out.println("Available denomination in ICICI: 2000, 1000, 200" );
     }
@@ -22,8 +22,9 @@ class HdfcBank{
         System.out.println("Available denomination in HDFC: 500, 200, 100" );
     }
 }
-//Main method
-class Main {
+
+//main class
+class  AbstractBankDenominationAssignment {
     public static void main(String[] args) {
         // creating object of class
         SbiBank sb = new SbiBank();
